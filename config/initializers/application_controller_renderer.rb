03c20f31a583d7138ec1f96ee6365c44f6d8e322
futa0904 <rinafuuta.0904@gmail.com>
2021-11-19 16:06:01 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '129e39ef04ca3c217dae06e8ee61dfc9e6b70fa7e6322680714d6b0f7f13dfee4505444d9b6c5d747ef1b90625169a960ed9bc1604d6a295568d415ed3451784'
